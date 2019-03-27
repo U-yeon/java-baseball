e4045ba58c3fd8d8d9b0ed0a3a8227ae1df1d666
U-Yeon <leanner@naver.com>
2019-03-27 15:06:08 +0900

    1) 메인함수의 시작

---
+++ NumberBaseball.java
@@ -0,0 +1,19 @@
+
+/**
+ * NumberBaseball.class
+ * 
+ * @author ¿©Áø
+ *2019.03.27
+ *
+ *
+ */
+import java.util.Scanner;
+
+
+public class NumberBaseball {
+		public static Scanner scan = new Scanner(System.in);
+		public static void main(String[] args) {
+			int userNum[] = new int[3];
+			int comNum[] = new int[3];
+		}
+}
