a15a426c46562a71a83a984c79c3b45d654beb8b
U-Yeon <leanner@naver.com>
2019-03-27 15:40:56 +0900

    7)main에서  반복 여부 확인, 최종

--- NumberBaseball.java
+++ NumberBaseball.java
@@ -16,8 +16,9 @@ public class NumberBaseball {
 			int userNum[] = new int[3];
 			int comNum[] = new int[3];
 			boolean result=false;
+			boolean continued=true;
 			
-			while(true) {
+			while(continued) {
 				comNum=getComNum();
 				
 				while(!result) {
@@ -34,6 +35,13 @@ public class NumberBaseball {
 				
 				System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
 				System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+				
+				if(scan.nextInt() == 2) {
+					continued=false;//게임 종료
+				}
+				else {
+					result=false;
+				}
 			}
 			
 			
