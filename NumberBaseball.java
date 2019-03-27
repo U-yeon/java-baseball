369e0b424ea4978463da8e6065102c09bb2e59ef
U-Yeon <leanner@naver.com>
2019-03-27 15:13:13 +0900

    2)getComNum-랜덤함수 생성 메소드

--- NumberBaseball.java
+++ NumberBaseball.java
@@ -15,5 +15,34 @@ public class NumberBaseball {
 		public static void main(String[] args) {
 			int userNum[] = new int[3];
 			int comNum[] = new int[3];
+			
+			while(true) {
+				comNum=getComNum();
+			}
+		}
+		
+		
+		/**랜덤함수 불러오는 함수*/
+		public static int[] getComNum() {
+			int comNum[] = new int[3];
+			comNum[0]=(int)(Math.random()*9+1);//1부터 9까지 랜덤
+			
+			while(true) {
+				comNum[1]=(int)(Math.random()*9+1);
+				
+				/**숫자 중복 여부 확인*/
+				if(comNum[1]!=comNum[0])
+					break;
+			}
+			
+			while(true) {
+				comNum[2]=(int)(Math.random()*9+1);
+				
+				/**숫자 중복 여부 확인*/
+				if((comNum[2]!=comNum[0])&&(comNum[2]!=comNum[1]))
+					break;
+			}
+			
+			return comNum;
 		}
 }
