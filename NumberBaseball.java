0c31d4bd73f223b4acc6c0a1b00539ef76421e56
U-Yeon <leanner@naver.com>
2019-03-27 15:20:03 +0900

    3)getUserNum-사용자의 숫자 입력 메소드

--- NumberBaseball.java
+++ NumberBaseball.java
@@ -18,11 +18,19 @@ public class NumberBaseball {
 			
 			while(true) {
 				comNum=getComNum();
+				
+				while(true) {
+					System.out.print("숫자를 입력해주세요 : ");
+					
+					userNum=getUserNum();
+				}
 			}
+			
+			
 		}
 		
 		
-		/**랜덤함수 불러오는 함수*/
+		/**랜덤함수 불러오는 메소드*/
 		public static int[] getComNum() {
 			int comNum[] = new int[3];
 			comNum[0]=(int)(Math.random()*9+1);//1부터 9까지 랜덤
@@ -44,5 +52,21 @@ public class NumberBaseball {
 			}
 			
 			return comNum;
+		}
+		
+		
+		/**사용자의 숫자 입력 메소드*/
+		public static int[] getUserNum() {
+			int userNum[] = new int[3];
+			
+			int input = scan.nextInt();
+			
+			/**세자리 숫자의 설정*/
+			userNum[0]=input/100;//100의 자리
+			input-=input/100*100;//100의 자리 날리기
+			userNum[1]=input/10;//10의 자리
+			userNum[2]=input%10;//1의 자리
+			
+			return userNum;
 		}
 }
